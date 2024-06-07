d8ef2669df914943b1aabd5ceb666ae31b6d0152
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-07 18:15:51 +1000

    Adding Exercise 5_2 - Creating metric versions of the calculation

---
+++ ex05/ex5_2.py
@@ -0,0 +1,25 @@
+""" More Variables and Printing """
+
+# Exercise 5_2 - More Variables and Printing
+# Adding a metric value for the variable names
+
+
+name = "Zed A. Shaw"
+age = 35  # not a lie
+height = 74  # Inches
+weight = 180  # lbs
+eyes = "Blue"
+teeth = "White"
+hair = "Brown"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not to heavy.")
+print(f"He's got my {eyes} and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"IMPERIAL: If I add {age}, {height}, and {weight} I get {total}")
+print(f"METRIC: If I add {age}, {height*2.54}, and {weight*0.45359237} I get {total}")
