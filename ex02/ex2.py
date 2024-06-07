4da6ce2ee6204bbaf7c9760372a2cb54fa81b215
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-07 17:16:14 +1000

    Adding Exercise 2 - Commenting and the use of Pound

---
+++ ex02/ex2.py
@@ -0,0 +1,13 @@
+""" Comments and Pound Characters """
+
+# example ex2.py
+
+# A comment, this is so you can read your program later.
+# Anything after # is ignored by Python.
+
+print("I could have code like this.")  # and the comment after is ignored.
+
+# You can also comment to "disable" or comment out code:
+# print("This won't run")
+
+print("This will run.")
