8c06d86113f3e7931f430184b84132a82d9ff4bb
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-07 16:50:30 +1000

    Create git repository for lpthw

---
+++ ex01/ex1.py
@@ -0,0 +1,11 @@
+""" This is Exercise 1 - Learning Python the Hard Way """
+
+# Exercise 1
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
