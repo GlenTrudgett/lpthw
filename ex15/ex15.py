1d5b7d27ce2aa5a3a369ce6238de5b035e002525
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-08 13:46:42 +1000

    Added the file close() commands as best practice.

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -11,9 +11,13 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 print(txt.read())
 
+# closing the file
+txt.close()
+
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
 print(txt_again.read())
+txt_again.close()  # closing this file too. Best practice
