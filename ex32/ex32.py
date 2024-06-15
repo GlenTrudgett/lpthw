e7149f4827c3af82ef7f9457c2307c2ea0e6074a
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-15 23:32:29 +1000

    updating firs part of the file

--- ex32/ex32.py
+++ ex32/ex32.py
@@ -1,3 +1,9 @@
 """ Loops and Lists """
 
 # Exercise 32 - Loops and Lists
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ["apples", "oranges", "pears", "apricots"]
+change = [1, "pennies", 2, "dimes", 3, "quarters"]
+
+# this first kind of for-loop goes through a list
