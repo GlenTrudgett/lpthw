3b468eea67a6af0bbd30e2f0249c7f2b0aefa8f0
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-16 14:10:56 +1000

    Additional code for Exercise 33

--- ex33/ex33.py
+++ ex33/ex33.py
@@ -1,3 +1,19 @@
 """ While Loops """
 
 # Exercise 33 - While Loops
+
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
