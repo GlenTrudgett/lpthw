d1f4f13a63d0dce6a30df2746d8cf3e7f3729a16
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-16 20:15:32 +1000

    added mystuff.py to learn about modules.

--- ex40/ex40a.py
+++ ex40/ex40a.py
@@ -1,6 +1,9 @@
 """ Modules, Classes, and Objects"""
 
 # Exercise 40a - Modules, Classes and Objects
+import mystuff
 
 mystuff = {"apple": "I AM APPLES!"}
 print(mystuff["apple"])
+
+mystuff.apple()
