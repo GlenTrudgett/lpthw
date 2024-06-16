b45fb1418f2da781330cfac1ae6c0abf8b1b9c6c
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-16 20:16:56 +1000

    Commented out the original code to demonstrate the module functionality

--- ex40/ex40a.py
+++ ex40/ex40a.py
@@ -3,7 +3,7 @@
 # Exercise 40a - Modules, Classes and Objects
 import mystuff
 
-mystuff = {"apple": "I AM APPLES!"}
-print(mystuff["apple"])
+# mystuff = {"apple": "I AM APPLES!"}
+# print(mystuff["apple"])
 
 mystuff.apple()
