b949aecf386f8285866543ec410dc6c2cae76a97
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-22 17:13:04 +1000

    added study question 2

---
+++ ex11/ex11_study2.py
@@ -0,0 +1,13 @@
+""" Asking Questions """
+
+# Exercise 11 (Study Question 2)- Asking Questions
+
+# you can get rid of the print statement and put the text in the ()
+
+age = input("How old are you? ")
+print("How tall are you?", end=" ")
+height = input()
+print("How much do you weigh?", end=" ")
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
