97cb4f4d7b406b55b601173e62e097f5e5f815b5
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-22 16:57:43 +1000

    Adding Study Question 3

---
+++ ex10/ex10_study3.py
@@ -0,0 +1,21 @@
+""" What was that? """
+
+# Exercise 10 (Study Question 3)- What was that?
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Gras
+\v\t* one down\n\a
+\999
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
