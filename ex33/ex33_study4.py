9e745bc640024f0f5ede7b5664be1387aaf336c0
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-23 16:27:06 +1000

    Performed Study Question 3,4 and 5 in one shot :)

---
+++ ex33/ex33_study4.py
@@ -0,0 +1,33 @@
+""" While Loops """
+
+# Exercise 33 (Study Question 1)- While Loops
+
+# Convert this while-loop to a function that you can call, and
+# replace 6 in the test (i<6) with a variable.
+
+
+# i = 0
+numbers = []
+
+
+def print_numbers(limit, step):
+    return list(range(0, limit, step))
+
+
+numbers = print_numbers(16, 4)
+
+print(numbers)
+
+
+# while i < 6:
+#    print(f"At the top i is {i}")
+#    numbers.append(i)
+#
+#    i = i + 1
+#    print("Numbers now: ", numbers)
+#    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
