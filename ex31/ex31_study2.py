a29ae3ef0153c478b0a036a29ca801fa3474cab5
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-23 15:34:20 +1000

    Finshed Study Questions 2 - Genetic and Biomech Olympics :)

--- ex31/ex31_study2.py
+++ ex31/ex31_study2.py
@@ -3,42 +3,57 @@
 # Exercise 31 (Study Question 2)- Making Decisions
 
 print(
-    """You enter a dark room with doors.
-Do you go through door #1 or door #2?"""
+    """The Genetic and Biomech Olympic 100m race is about to start.
+You need to make a decision on which augmentation you will choose.
+Do you use:\n\t(1) - Genetically Enhanced Heamoglobin (GEH)
+    \t(2) - Carbon struts (CS)
+    \t(3) - Good old fashioned Cocaine (aka. Speed)??
+    """
 )
 
 door = input("> ")
 
 if door == "1":
-    print("There's a giant bear here eating a cheese cake.")
-    print("What do you do?")
-    print("1. Take the cake.")
-    print("2. Scream at the bear.")
+    print(
+        "Genetically Enhanced Heamoglobion (GEH) is highly volatile and Experimental."
+    )
+    print("How much do you administer?")
+    print("(1) 250ml Take the cake.")
+    print("(2) A full blood transfusion.")
 
-    bear = input("> ")
+    geh = input("> ")
 
-    if bear == 1:
-        print("The bear eats your face off. Good job!")
-    elif bear == 2:
-        print("The bear eats your legs off. Good job!")
+    if geh == "1":
+        print(
+            "You decide to take a calculated risk. Your organism comes 3rd! Good job!"
+        )
+    elif geh == "2":
+        print("We're not here to make friends. The first 30m is incredible, then")
+        print(
+            "the organism internally combusts. Spraying biological ooze everywhere. Good job!"
+        )
     else:
-        print(f"Well, doing {bear} is probably better.")
-        print("Bear runs away.")
+        print(f"Well, doing {geh} is probably a bad idea.")
+        print("You are disqualified.")
 
 elif door == "2":
-    print("You stare into the endless abyss at Cthulu's retina.")
-    print("1. Blueberries.")
-    print("2. Yellow jacket clothespins.")
-    print("3. Understanding revolvers yelling melodies.")
+    print("The Carbon Struts(CS) provide high tensile strength and greater leverage.")
+    print("(1) 2 metre carbon struts?")
+    print("(2) 1 metre carbon struts?")
 
-    insanity = input("> ")
+    cs = input("> ")
 
-    if insanity == "1" or insanity == "2":
-        print("Your body survives powered by a mind of jello.")
-        print("Good job!")
+    if cs == "1":
+        print(
+            "The 2 Metre struts.... well you vault in two strides out of the field and are"
+        )
+        print("seen passing traffic down the highway 6 km away. Whoops :-/ ")
+    elif cs == "2":
+        print("The 1 Metre struts is an excellent choice! The slightly extra leverage")
+        print("provides a balance of stride length and strength. You WON!!!!")
     else:
-        print("The insanity rots your eyes into a pool of muck.")
+        print("This insanity is a very bad idea. You're disqualified.")
         print("Good job!")
-
 else:
-    print("You stumble around and fall on a knife and die.  Good job!")
+    print("You are an evil and diabolical scientist with not respect for the Olympics!")
+    print("Good day to you sir/madam.")
