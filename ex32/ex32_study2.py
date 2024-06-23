fc8681bb33f5266a6cc1be88224e09f11c8e0e15
GlenTrudgett <cerebralpanic@gmail.com>
2024-06-23 16:04:08 +1000

    Added Study Questions 2 & 3

---
+++ ex32/ex32_study2.py
@@ -0,0 +1,16 @@
+""" Loops and Lists """
+
+# Exercise 32 (study question 2) - Loops and Lists
+
+# Could you have avoided that for-loop entirely on line 22 and just assigned
+# range(0,6) directly to the elements?
+
+# let's try!!
+
+elements = []
+print(elements)
+elements = range(0, 6)
+print(elements)
+
+
+# This doesn't work and you need to use the for-loop to iterate through.
